bf1652ef5ef5e8a3896b4ebfc312ccdb7fe712c9
Raymond Ehizoba <rayehizoba@gmail.com>
2018-02-23 00:59:19 +0100

    Feat: Scaffold database seeder

--- api/services/db.service.js
+++ api/services/db.service.js
@@ -1,96 +1,102 @@
 const database = require('../../config/database');
+const dbSeeder = require('./dbSeeder.service');
+const environment = require('./environment.service');
 
-const dbService = (environment, migrate) => {
+const dbService = (migrate) => {
   const authenticateDB = () => (
     database
-      .authenticate()
+    .authenticate()
   );
 
   const dropDB = () => (
     database
-      .drop()
+    .drop()
   );
 
   const syncDB = () => (
     database
-      .sync()
+    .sync()
   );
 
-  const successfulDBStart = () => (
-    console.info('connection to the database has been established successfully')
-  );
+  const successfulDBStart = () => {
+    console.info('connection to the database has been established successfully');
+    if (environment.isNotProduction) {
+      dbSeeder.start();
+    }
+  };
 
   const errorDBStart = (err) => (
     console.info('unable to connect to the database:', err)
   );
 
   const wrongEnvironment = () => {
-    console.warn(`only development, staging, test and production are valid NODE_ENV variables but ${environment} is specified`);
+    console.warn(`only development, staging, test and production are valid NODE_ENV variables but ${environment.name} is specified`);
     return process.exit(1);
   };
 
   const startMigrateTrue = () => (
     syncDB()
-      .then(() => successfulDBStart())
-      .catch((err) => errorDBStart(err))
+    .then(() => successfulDBStart())
+    .catch((err) => errorDBStart(err))
   );
 
   const startMigrateFalse = () => (
     dropDB()
-      .then(() => (
+    .then(() => (
         syncDB()
-          .then(() => successfulDBStart())
-          .catch((err) => errorDBStart(err))
+        .then(() => successfulDBStart())
+        .catch((err) => errorDBStart(err))
       )
-        .catch((err) => errorDBStart(err)),
-      )
+      .catch((err) => errorDBStart(err)),
+    )
   );
 
   const startDev = () => (
     authenticateDB()
-      .then(() => {
-        if (migrate) {
-          return startMigrateTrue();
-        }
+    .then(() => {
+      if (migrate) {
+        return startMigrateTrue();
+      }
 
-        return startMigrateFalse();
-      })
+      return startMigrateFalse();
+    })
   );
 
   const startStage = () => (
     authenticateDB()
-      .then(() => {
-        if (migrate) {
-          return startMigrateTrue();
-        }
+    .then(() => {
+      if (migrate) {
+        return startMigrateTrue();
+      }
 
-        return startMigrateFalse();
-      })
+      return startMigrateFalse();
+    })
   );
 
   const startTest = () => (
     authenticateDB()
-      .then(() => startMigrateFalse())
+    .then(() => startMigrateFalse())
   );
 
   const startProd = () => (
     authenticateDB()
-      .then(() => startMigrateFalse())
+    .then(() => startMigrateFalse())
   );
 
   const start = () => {
-    switch (environment) {
-      case 'development':
-        return startDev();
-      case 'staging':
-        return startStage();
-      case 'test':
-        return startTest();
-      case 'production':
-        return startProd();
-      default:
-        return wrongEnvironment();
+    if (environment.isDevelopment) {
+      return startDev();
     }
+    if (environment.isStaging) {
+      return startStage();
+    }
+    if (environment.isTesting) {
+      return startTest();
+    }
+    if (environment.isProduction) {
+      return startProd();
+    }
+    return wrongEnvironment();
   };
 
   return {
---
+++ api/services/dbSeeder.service.js
@@ -0,0 +1,6 @@
+module.exports = {
+  start: () => {
+    console.info('seeding the database with fake data')
+
+  }
+};
