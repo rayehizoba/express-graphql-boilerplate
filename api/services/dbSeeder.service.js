497d9e526b81c45e38d1e0672ffa1a3ee0955726
Raymond Ehizoba <rayehizoba@gmail.com>
2018-02-23 02:49:35 +0100

    Feat: dbSeeder.service can now generateUsers for every generateUsers

--- api/services/dbSeeder.service.js
+++ api/services/dbSeeder.service.js
@@ -1,19 +1,31 @@
 const faker = require('faker');
 const User = require('../models/User/User');
+const Post = require('../models/Post/Post');
+
+const genetatePosts = (id, count = 3) => {
+  for (let i = 0; i < count; i + 1) {
+    Post.create({
+      UserId: id,
+      post: faker.lorem.paragraph,
+    });
+  }
+};
 
 const generateUsers = (count = 3) => {
-  for (let i = 0; i < count; i++) {
+  for (let i = 0; i < count; i + 1) {
     User.create({
       username: faker.name.findName(),
       email: faker.internet.email(),
       password: faker.internet.password(),
+    }).then((newUser) => {
+      genetatePosts(newUser.id);
     });
   }
-}
+};
 
 module.exports = {
   start: () => {
-    console.info('[dbSeeder.service] seeding the database with fake data')
+    console.info('[dbSeeder.service] seeding the database with fake data');
     generateUsers();
-  }
+  },
 };
